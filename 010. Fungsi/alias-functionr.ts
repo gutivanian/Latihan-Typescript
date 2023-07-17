acbc0a4956164465fbaf07bdabae515f35579812
gutivanian <gutivanaliefs@gmail.com>
2023-07-17 21:25:26 +0700

    01

---
+++ 010. Fungsi/alias-functionr.ts
@@ -0,0 +1,4 @@
+type Negate = (value: number) => number;
+
+// in this function, the parameter `value` automatically gets assigned the type `number` from the type `Negate`
+const negateFunction: Negate = (value) => value * -1;
