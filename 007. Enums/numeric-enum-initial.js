0f9dbecd572f7a62a3fcab79f51901294786e39a
gutivanian <gutivanaliefs@gmail.com>
2023-09-23 14:08:29 +0700

    01

--- 007. Enums/numeric-enum-initial.js
+++ 007. Enums/numeric-enum-initial.js
@@ -1,11 +1,10 @@
-num;
-CardinalDirections;
-{
-    North = 1,
-        East,
-        South,
-        West;
-}
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections[CardinalDirections["North"] = 1] = "North";
+    CardinalDirections[CardinalDirections["East"] = 2] = "East";
+    CardinalDirections[CardinalDirections["South"] = 3] = "South";
+    CardinalDirections[CardinalDirections["West"] = 4] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
 // logs 1
 console.log(CardinalDirections.North);
 // logs 4
