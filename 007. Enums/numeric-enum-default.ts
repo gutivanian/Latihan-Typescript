e4fa2ef4d87312039cfb30f376c8d4d97d272728
gutivanian <gutivanaliefs@gmail.com>
2023-09-29 14:04:05 +0700

    01

--- 007. Enums/numeric-enum-default.ts
+++ 007. Enums/numeric-enum-default.ts
@@ -11,4 +11,6 @@ let currentDirection = CardinalDirection.East;
 console.log(currentDirection);
 
 currentDirection = 2
+// currentDirection = "North"
+// kode diatas akan memberikan error, karena North dalam bentuk string bukan enum, yang enum adalah angka indeks 1,2,3, dst
 console.log(currentDirection);
