a69e518c5308a19ec918d3ca9305ec8a6d2c58e6
gutivanian <gutivanaliefs@gmail.com>
2023-10-14 11:55:34 +0700

    01

---
+++ 006. Object/non-optional-property.ts
@@ -0,0 +1,4 @@
+const car: { type: string, mileage: number } = { // Error: Property 'mileage' is missing in type '{ type: string; }' but required in type '{ type: string; mileage: number; }'.
+  type: "Toyota",
+};
+car.mileage = 2000;
