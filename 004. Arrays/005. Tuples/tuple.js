96d893b83a8f630fa043acb0e6fbc09c08419777
gutivanian <gutivanaliefs@gmail.com>
2023-11-10 00:07:57 +0700

    01

---
+++ 004. Arrays/005. Tuples/tuple.js
@@ -0,0 +1,4 @@
+// define our tuple
+var ourTuple;
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
---
+++ 004. Arrays/005. Tuples/tuple.ts
@@ -0,0 +1,5 @@
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
