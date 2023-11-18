36f6f85fe3c092bdbcdff97d7ef6cbd18b34735d
gutivanian <gutivanaliefs@gmail.com>
2023-11-18 23:55:48 +0700

    01

---
+++ 004. Arrays/read-only.ts
@@ -0,0 +1,3 @@
+const names: readonly string[] = ["Dylan"];
+names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// try removing the readonly modifier and see if it works?
