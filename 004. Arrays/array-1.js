b9adebe1a20fba2b8c3c79f254b4bbf63433c7b0
gutivanian <gutivanaliefs@gmail.com>
2024-06-17 23:51:47 +0700

    01

---
+++ 004. Arrays/array-1.js
@@ -0,0 +1,7 @@
+var names = [];
+names.push("Dylan"); // no error
+// names.push(3); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+var angka = [1, 2, 3];
+angka.push(10);
+console.log(names);
+console.log(angka);
